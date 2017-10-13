9df8f3d8e38045bf0c9211e056293b8dff0d1d74
TeusoftSuperman <khoaha@teusoft.dk>
2017-10-13 11:09:25 +0700

    with draft change

--- src/Exam1.kt
+++ src/Exam1.kt
@@ -5,7 +5,9 @@
 fun main(args: Array<String>) {
     println("Hello to the World The Golden Circle")
     println("This is cool feature #2.1")
+    println("This is cool feature #2.2")
 
     println("Hello to the World. Feature #2.1")
     log("Search product. New Change #2.1")
+    log("Search product. New Change #2.2")
 }
