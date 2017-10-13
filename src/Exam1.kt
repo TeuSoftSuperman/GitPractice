26fa1c1694ca7c28f49d3e615ba9f0456cff2873
TeusoftSuperman <khoaha@teusoft.dk>
2017-10-13 10:34:51 +0700

    hot fix feature 2
    (cherry picked from commit 7e5e903)

--- src/Exam1.kt
+++ src/Exam1.kt
@@ -4,13 +4,8 @@
 
 fun main(args: Array<String>) {
     println("Hello to the World The Golden Circle")
-    println("This is cool feature #1")
-    println("This is cool feature #2")
+    println("This is cool feature #2.1")
 
-    println("Hello to the World. Feature #1")
-    log("Adding new product. New Change #11")
-    log("Update product. New Change #22")
-    log("Delete product. New Change #33")
-
-    log("Search product. New Change #44")
+    println("Hello to the World. Feature #2.1")
+    log("Search product. New Change #2.1")
 }
