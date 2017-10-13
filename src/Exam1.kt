6110926b2f63443deeddd44a30bdb94edb55682e
KhoaHoang <hoanganhkhoa1012@gmail.com>
2017-10-13 10:16:50 +0700

    Add cool feature 1

--- src/Exam1.kt
+++ src/Exam1.kt
@@ -4,7 +4,9 @@
 
 fun main(args: Array<String>) {
     println("Hello to the World The Golden Circle")
+    println("This is cool feature #1")
 
+    println("Hello to the World. Feature #1")
     log("Adding new product. New Change #11")
     log("Update product. New Change #22")
     log("Delete product. New Change #33")
