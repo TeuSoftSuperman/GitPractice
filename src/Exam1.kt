9d64a56bff0088a2f01d3ff6fb2164622c8cebb2
TeusoftSuperman <khoaha@teusoft.dk>
2017-10-13 09:46:32 +0700

    master change #1

--- src/Exam1.kt
+++ src/Exam1.kt
@@ -3,7 +3,11 @@
  */
 
 fun main(args: Array<String>) {
-    println("Hello to the World")
-    log("Adding new product")
-    log("Update product")
+    log("Master change the thing #1")
+
+    println("Hello to the World #1")
+    log("Adding new product #1")
+    log("Update product #1")
+
+    log("Master change the thing #1")
 }
